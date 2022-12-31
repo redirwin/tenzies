550c5ae5e9b252e6131b41e90ce4a43a5ec95c48
David Irwin <redirwin@gmail.com>
2022-12-31 01:00:40 -0700

    Tracks lowest rolls across games using local storage.

--- src/Components/scripts/App.jsx
+++ src/Components/scripts/App.jsx
@@ -1,64 +1,107 @@
-import { useState, useEffect } from 'react'
-import { nanoid } from 'nanoid'
-import Confetti from 'react-confetti'
-import styles from '../styles/App.module.scss'
-import Die from './Die'
+import { useState, useEffect } from "react";
+import { nanoid } from "nanoid";
+import Confetti from "react-confetti";
+import styles from "../styles/App.module.scss";
+import Die from "./Die";
 
 export default function App() {
-  const [dice, setDice] = useState(getAllNewDice)
-  const [tenzies, setTenzies] = useState(false)
+  const [rolls, setRolls] = useState(0);
+  const [showInstructions, setShowInstructions] = useState(true);
+  const [dice, setDice] = useState(getAllNewDice);
+  const [tenzies, setTenzies] = useState(false);
+  const [stats, setStats] = useState(getStats());
 
   useEffect(() => {
-    const allDiceSame = dice.every(die => die.value === dice[0].value)
-    const allDiceHeld = dice.every(die => die.isHeld)
+    const allDiceSame = dice.every((die) => die.value === dice[0].value);
+    const allDiceHeld = dice.every((die) => die.isHeld);
     if (allDiceSame && allDiceHeld) {
-      setTenzies(true)
-      console.log('Tenzies!')
+      setTenzies(true);
+      updateStats();
     }
-  }, [dice])
+  }, [dice]);
+
+  function getStats() {
+    return {
+      lowestRolls: localStorage.getItem("lowestRolls")
+        ? parseInt(localStorage.getItem("lowestRolls"))
+        : null,
+    };
+  }
+
+  function updateStats() {
+    if (stats.lowestRolls === null || rolls < stats.lowestRolls) {
+      localStorage.setItem("lowestRolls", rolls);
+      setStats((prevStats) => ({ ...prevStats, lowestRolls: rolls }));
+    }
+  }
 
   function getAllNewDice() {
-    const dice = []
+    const dice = [];
     for (let i = 0; i < 10; i++) {
       dice.push({
         id: nanoid(),
-        value: Math.ceil(Math.random() * 6),
-        isHeld: false
-      })
+        value: rolls === 0 ? "" : Math.ceil(Math.random() * 6),
+        isHeld: false,
+      });
     }
-    return dice
+    return dice;
   }
 
   function rollDice() {
     if (tenzies) {
-      setTenzies(false)
-      setDice(getAllNewDice)
-      return
+      setTenzies(false);
+      setDice(getAllNewDice);
+      setRolls(0);
+      setShowInstructions(false);
+      return;
     }
-    setDice(prevDice => prevDice.map((die) => {
-      return die.isHeld ? die : {...die, value: Math.ceil(Math.random() * 6)}
-    } ))
+    setDice((prevDice) =>
+      prevDice.map((die) => {
+        return die.isHeld
+          ? die
+          : { ...die, value: Math.ceil(Math.random() * 6) };
+      })
+    );
+    setRolls((prevRolls) => prevRolls + 1);
   }
 
   function holdDice(id) {
-    setDice (prevDice => prevDice.map((die) => {
-      return die.id === id ? {...die, isHeld: !die.isHeld} : die
-    }))
+    setDice((prevDice) =>
+      prevDice.map((die) => {
+        return die.id === id ? { ...die, isHeld: !die.isHeld } : die;
+      })
+    );
   }
 
-  const diceElements = dice.map(die => <Die key={die.id} {...die} holdDice={holdDice}/>)
+  const diceElements = dice.map((die) => (
+    <Die key={die.id} {...die} holdDice={holdDice} />
+  ));
 
   return (
     <main className={styles.App}>
       {tenzies && <Confetti />}
       <header>
         <h1>Tenzies</h1>
-        <p>Roll until all dice are the same. Click each die to freeze it at its current value between rolls.</p>
+        {rolls === 0 && showInstructions === true ? (
+          <p>
+            Roll until all dice are the same. Click or tap each die to freeze it
+            at its current value between rolls.
+          </p>
+        ) : (
+          <div className={styles.flexDiv}>
+            <div>Lowest Rolls: {stats.lowestRolls}</div>
+            <div>Current Rolls: {rolls}</div>
+          </div>
+        )}
       </header>
-      <section className={styles.dice_container}>
-        {diceElements}
-      </section>
-      <button onClick={rollDice}>{tenzies ? "New Game" : "Roll 'em!"}</button>
+      {rolls > 0 && (
+        <section className={styles.dice_container}>{diceElements}</section>
+      )}
+      <div>
+        <button onClick={rollDice}>
+          {tenzies ? "New Game" : rolls === 0 ? "Let's Roll!" : "Roll again!"}
+        </button>
+      </div>
     </main>
-  )
+  );
 }
--- src/Components/scripts/Die.jsx
+++ src/Components/scripts/Die.jsx
@@ -1,13 +1,12 @@
-import styles from '../styles/Die.module.scss';
+import styles from "../styles/Die.module.scss";
 
-export default function Die(props) { 
-
-    return (
-        <div 
-            className={`${styles.Die} ${props.isHeld && styles.held}`}
-            onClick={() => props.holdDice(props.id)}
-        >
-            <span>{props.value}</span>
-        </div>
-    )
+export default function Die(props) {
+  return (
+    <div
+      className={`${styles.Die} ${props.isHeld && styles.held}`}
+      onClick={() => props.holdDice(props.id)}
+    >
+      <span>{props.value}</span>
+    </div>
+  );
 }

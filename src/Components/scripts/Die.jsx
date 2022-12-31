6af4982f2acce9d5f72f748afcb866345be30595
David Irwin <redirwin@gmail.com>
2022-12-31 15:41:00 -0700

    Updates dice text digit to pips created with CSS.

--- src/Components/scripts/Die.jsx
+++ src/Components/scripts/Die.jsx
@@ -6,7 +6,12 @@ export default function Die(props) {
       className={`${styles.Die} ${props.isHeld && styles.held}`}
       onClick={() => props.holdDice(props.id)}
     >
-      <span>{props.value}</span>
+      {props.value === 1 && <span className={`${styles.cssDice} ${styles.cssDice_1}`}></span>}
+      {props.value === 2 && <span className={`${styles.cssDice} ${styles.cssDice_2}`}></span>}
+      {props.value === 3 && <span className={`${styles.cssDice} ${styles.cssDice_3}`}></span>}
+      {props.value === 4 && <span className={`${styles.cssDice} ${styles.cssDice_4}`}></span>}
+      {props.value === 5 && <span className={`${styles.cssDice} ${styles.cssDice_5}`}></span>}
+      {props.value === 6 && <span className={`${styles.cssDice} ${styles.cssDice_6}`}></span>}
     </div>
   );
 }

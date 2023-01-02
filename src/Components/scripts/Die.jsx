86c0fff058e1b73e5b25c5202f9d138aea8f6996
David Irwin <redirwin@gmail.com>
2023-01-01 22:48:07 -0700

    Simplifies return statement and updates state to use value prop.

--- src/Components/scripts/Die.jsx
+++ src/Components/scripts/Die.jsx
@@ -1,17 +1,23 @@
+import { useState, useEffect } from "react";
 import styles from "../styles/Die.module.scss";
 
 export default function Die(props) {
+  const [dieFace, setDieFace] = useState()
+
+  const updateDieFace = () => {
+    setDieFace(props.value)
+  }
+
+  useEffect(updateDieFace, [props.value])
+
   return (
     <div
       className={`${styles.Die} ${props.isHeld && styles.held}`}
       onClick={() => props.holdDice(props.id)}
     >
-      {props.value === 1 && <span className={`${styles.cssDice} ${styles.cssDice_1}`}></span>}
-      {props.value === 2 && <span className={`${styles.cssDice} ${styles.cssDice_2}`}></span>}
-      {props.value === 3 && <span className={`${styles.cssDice} ${styles.cssDice_3}`}></span>}
-      {props.value === 4 && <span className={`${styles.cssDice} ${styles.cssDice_4}`}></span>}
-      {props.value === 5 && <span className={`${styles.cssDice} ${styles.cssDice_5}`}></span>}
-      {props.value === 6 && <span className={`${styles.cssDice} ${styles.cssDice_6}`}></span>}
+
+    <span className={`${styles.cssDice} ${styles[`cssDice_${dieFace}`]}`}></span>
+
     </div>
   );
 }
